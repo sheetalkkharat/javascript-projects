c11801611d4665300349ac8025c73d6f95fc7e03
sheetalkkharat <sheetalkharatk@gmail.com>
2024-05-08 14:25:02 -0500

    changes commited in p1

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,27 @@
 let num = 1001;
 
 //Returns 'undefined'.
-console.log(num.length);
+//console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+let num1 = String(num)
+console.log(typeof num1)
+console.log(num1)
+console.log(num1.length)
+
+
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
 
+num = 123.45
+let num2 = String(num)
+console.log(num2.length)
+
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+num = 123.45
+if(!isNaN(num)) {
+ console.log('It is a number!');
+}else {
+ console.log(" Not a Number.");
+ 
+}
