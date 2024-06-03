34fca1d74b9f60243ebed35c8fe0a1578c35fed9
sheetalkkharat <sheetalkharatk@gmail.com>
2024-06-03 18:26:30 -0500

    object exercise

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -10,9 +10,7 @@ function findMinValue(arr){
 
 //Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
 //Sample arrays for testing:
-let nums1 = [5, 10, 2, 42];
-let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
-let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
 
 function sortNumbersArray(array) {
   let sortedArray = [];
