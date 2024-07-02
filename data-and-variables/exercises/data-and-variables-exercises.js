d79b0d339d3e2a0727acb5a4e9df84c5945dcdd9
sheetalkkharat <sheetalkharatk@gmail.com>
2024-07-01 19:02:40 -0500

    Redoing exercise1

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -30,7 +30,7 @@ let hoursToMars = milesToMars / shuttleSpeed;
 let daysToMars = hoursToMars / 24;
 
 // Print the results of the space mission calculations below
-console.log(nameOfTheSpaceShuttle," will take" ,daysToMars," days to reach Mars.")
+console.log(`${nameOfTheSpaceShuttle} will take ${daysToMars}days to reach Mars.`)
 
 // Calculate a trip to the moon below
 let milesToMoon = distanceToTheMoon * milesPerkilometer;
@@ -38,4 +38,4 @@ let hoursToMoon = milesToMoon / shuttleSpeed;
 let daysToMoon = hoursToMoon / 24;
 
 // Print the results of the trip to the moon below
-console.log(nameOfTheSpaceShuttle," will take", daysToMoon,"days to reach Moon.")
+console.log(`${nameOfTheSpaceShuttle} will take ${daysToMoon}days to reach Moon.`)
