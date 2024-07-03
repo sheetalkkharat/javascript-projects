dd52df5cc253d1d95551b3b8eda7dee0f1c31b5a
sheetalkkharat <sheetalkharatk@gmail.com>
2024-07-03 18:04:53 -0500

    exercise string attempt 2

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -14,14 +14,16 @@ console.log(num1.length)
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
 
 num = 123.45
-let num2 = String(num)
+let num2 = num.toString().replace('.','');
 console.log(num2.length)
 
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
 num = 123.45
 if(!isNaN(num)) {
- console.log('It is a number!');
+    let numCon = num.toString();
+    if(numCon.includes('.'))
+ console.log('It is a  decimal number!');
 }else {
- console.log(" Not a Number.");
+ console.log(" It is a integer Number.");
  
 }
