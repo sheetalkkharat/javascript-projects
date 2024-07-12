dd306a3970ad2263b4b2643c86ec6aa1bd85ea0f
sheetalkkharat <sheetalkharatk@gmail.com>
2024-07-11 20:18:42 -0500

    studio for classes

--- classes/studio/ClassStudio.js
+++ classes/studio/ClassStudio.js
@@ -1,8 +1,50 @@
-//Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
 
 
 
-//Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
+class CrewCandidate {
+    constructor(name, mass, scores) {
+        this.name = name;
+        this.mass = mass;
+        this.scores = scores;
+    }
+
+    
+    addScore(newScore) {
+        this.scores.push(newScore);
+        console.log(this.scores);
+    }
+
+
+    average() {
+        let total = 0;
+        for (let i = 0; i < this.scores.length; i++) {
+            total += this.scores[i];
+        }
+        return total;
+    }
+}
+
+
+
+
+
+let candidate1 = new CrewCandidate('Bubba Bear', 135, [88, 85, 90]);
+let candidate2 = new CrewCandidate('Merry Maltese', 1.5, [93, 88, 97]);
+let candidate3 = new CrewCandidate('Glad Gator', 225, [75, 78, 62]);
+
+console.log(candidate1);
+console.log(candidate2);
+console.log(candidate3);
+
+
+candidate1.addScore(93);
+console.log(average.candidate1);
+
+
+
+
+
+
 
 
 
