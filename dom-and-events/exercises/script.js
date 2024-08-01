7b927f383d5ca13964e93f830d562506b32c895a
sheetalkkharat <sheetalkharatk@gmail.com>
2024-07-31 21:55:31 -0500

    exercise DOM & eve

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,26 @@
-function init () {
+function init() {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener('mouseover',function( event ) {
+        event.target.style.backgroundColor = "red";
+    })
+
+    missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
 }
+
+// missionAbort.addEventListener('click', event => {
+//     paragraph.innerHTML = 'Are you sure you want to abort the mission?';
+
+// });
+
 
 window.addEventListener("load", init);
